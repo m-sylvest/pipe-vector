3bd4339403f17d3aff4c72ff79cb83083a94efde
m-sylvest <mail@mikael-sylvest.dk>
2017-06-17 12:18:40 +0200

    seq() added

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include <functional>
 #include <vector>
 #include <list>
+#include <algorithm>
 
 using namespace std;
 
 template <template<typename,typename> typename C, typename T, typename A, typename U>
-auto operator >> ( const C<T,A> &v, function<U(T)> f)
+auto operator | ( const C<T,A> &v, function<U(T)> f)
 {
   C<U,A> result;
   for( const auto e : v )
@@ -15,23 +16,28 @@ auto operator >> ( const C<T,A> &v, function<U(T)> f)
   return result;
 }
 
+
+vector<int> seq( int count )
+{
+	// class generator:
+	struct c_unique {
+		int current;
+		c_unique() {current=0;}
+		int operator()() {return ++current;}
+	} UniqueNumber;
+
+  vector<int> result( count );
+  generate_n( result.begin(), count, UniqueNumber );
+  return result;
+}
+
 int main( int argc, char *argv[] )
 {
-  list<int> a = {1,2,3};
+  auto a = seq(3);
   
-  function<int(int)> f = [](int x) { return x+7; } ;
+  function<  int(int)> f = [](int x) { return x+7; } ;
   function<float(int)> g = [](int x) { return x*4 + 0.5; } ;
   
-  auto b = a >> f >> g;
-  
-  for( auto x : b )
-    cout << x << endl;
-#if 0    
-  auto c = a >> [](int x) -> float { return (x+7)*4 - 0.5; };
-  for( auto x : c )
-    cout << x << endl;  
-#endif  
-
-  for( auto x: a >> f >> g )
+  for( auto x: a | f | g )
     cout << x << endl;
 }
