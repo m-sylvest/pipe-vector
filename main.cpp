a87e48d8f003d87739feab7aea31eb8ee9d3b0ff
m-sylvest <mail@mikael-sylvest.dk>
2017-06-18 23:15:59 +0200

    date.h + days_in_year/month added

--- main.cpp
+++ main.cpp
@@ -4,32 +4,59 @@
 #include <list>
 #include <algorithm>
 
-using namespace std;
+#include "date.h"
 
-template <template<typename,typename> typename C, typename T, typename A, typename U>
-auto operator | ( const C<T,A> &v, function<U(T)> f)
+using namespace std;
+using namespace std::chrono;
+using namespace date;
+
+template <template<typename T,typename ... A> typename C, typename T, typename ... A, typename U>
+auto operator | ( const C<T,A...> &v, function<U(T)> f)
 {
-  C<U,A> result;
+  C<U,A...> result;
   for( const auto e : v )
     result.push_back( f(e) );
     
   return result;
 }
 
-
-vector<int> seq( int count )
+vector<int> seq( int from, int to )
 {
 	// class generator:
-	struct c_unique {
+	struct UniqueNumber {
 		int current;
-		c_unique() {current=0;}
+		UniqueNumber( int from=1 ) {current=from-1;}
 		int operator()() {return ++current;}
-	} UniqueNumber;
+	};
 
-  vector<int> result( count );
-  generate_n( result.begin(), count, UniqueNumber );
+  vector<int> result( to-from+1 );
+  generate_n( result.begin(), to-from+1, UniqueNumber(from) );
   return result;
 }
+
+vector<int> seq( int count ) { return seq(1,count); }
+
+days days_in_year( unsigned short y )
+{
+  auto next = sys_days{year{y+1}/1/1};
+  auto curr = sys_days{year{ y }/1/1};
+
+  return next - curr;
+}
+
+days days_in_month( unsigned short y, unsigned char m )
+{
+  auto next = sys_days{year{y}/month{m}/last};
+  auto curr = sys_days{year{y}/month{m}/0};
+
+  return next - curr;
+}
+
+
+
+
+
+
 
 int main( int argc, char *argv[] )
 {
@@ -40,4 +67,13 @@ int main( int argc, char *argv[] )
   
   for( auto x: a | f | g )
     cout << x << endl;
+
+	auto today = floor<days>(system_clock::now());
+	cout << today << '\n';
+
+  cout << days_in_year(  2016  ) << endl;
+  cout << days_in_month(2016, 2) << endl;
+  cout << days_in_month(2016, 1) << endl;
+  cout << days_in_month(2017, 2) << endl;
 }
+
